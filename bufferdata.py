e605941876c98842c449cafb59bd56f542b7f240
lucas darroch <darroch.lucas@gmail.com>
2023-11-15 03:40:44 -0500

    troubleshooting on remote

--- bufferdata.py
+++ bufferdata.py
@@ -54,7 +54,7 @@ while True:
                     f.close()   
     # If there are more than 30 minutes worth of data points in the buffer, delete the oldest data point
     buffer = np.genfromtxt('DATA/LJbuffer.csv', delimiter=';', names=True)
-    print(buffer.size)
+    print(buffer.size,bufferSize/avgTime)
     if buffer.size > bufferSize/avgTime:
         buffer = np.delete(buffer,0)       
 
