1950483230dbfd3f4baa6f355b8ffe5a0e4e1fe7
lucas darroch <darroch.lucas@gmail.com>
2023-11-15 02:45:05 -0500

    restructured the buffer. First test.

--- bufferdata.py
+++ bufferdata.py
@@ -2,54 +2,20 @@ import os
 import time
 import csv
 import numpy as np
+from readLJ import buffer_output
 
 # Delete the buffer file if it exists
 if os.path.exists('DATA/LJbuffer.csv'):
     os.remove('DATA/LJbuffer.csv')
 
-# Read the header names from LJdata.csv
-header = np.genfromtxt('DATA/LJdata.csv', delimiter=';', names=True).dtype.names
+# Open the data file and grab the header
+data = np.genfromtxt('DATA/LJdata.csv', delimiter=';', names=True)
+header = data.dtype.names
 
-# Initialize circular buffer with a fixed size of 1800
-circular_buffer = deque(maxlen=1800)
-
-# Read the second row of the file into an array
-with open('DATA/LJdata.csv', 'r') as f:
-    next(f)  # skip first row
-    data = next(f).strip().split(';')
-
-# Initialize variables for averaging
-sum_data = [0] * len(data)
-num_data = 0
-start_time = time.time()
-
-# Write header names to buffer csv
-with open('DATA/LJbuffer.csv', mode='w') as file:
-    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+# Open the buffer file and write the header
+with open('DATA/LJbuffer.csv','w') as f:
+    writer = csv.writer(f, delimiter=';')
     writer.writerow(header)
 
-while True:
-    # Append the array to the circular buffer
-    circular_buffer.append(data)
-
-    # Add the data to the sum for averaging
-    for i in range(len(data)):
-        sum_data[i] += float(data[i])
-    num_data += 1
-
-    # If 10 seconds have passed, calculate the average and print it
-    if time.time() - start_time >= 10:
-        avg_data = [sum_data[i] / num_data for i in range(len(data))]
-        with open('DATA/LJbuffer.csv', mode='a') as file:
-            writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(avg_data)
-        sum_data = [0] * len(data)
-        num_data = 0
-        start_time = time.time()
-
-    # If the circular buffer is full, remove the oldest element from the buffer
-    if len(circular_buffer) == 1800:
-        data = circular_buffer.popleft()
-
-    # Wait for 1 second before repeating
-    time.sleep(1)
+# Define scanRate and avgTime
+scanRate, avgTime = buffer_output()[3], buffer_output()[4]
