df8632badc550fc0053ec8cbd8222c4d3e068341
lucas darroch <darroch.lucas@gmail.com>
2023-12-21 04:26:14 -0500

    debug

--- autovalve.py
+++ autovalve.py
@@ -62,6 +62,7 @@ if __name__ == '__main__':
             ts[0] = dt.datetime.utcnow().timestamp()
             try:
                 buff,strm = loadData(buffr,strmr)
+                print(buff,strm)
                 overfill = strm['%s'%closeSensor[0]][-1]
                 fill = strm['%s'%closeSensor[1]][-1]
             except:
